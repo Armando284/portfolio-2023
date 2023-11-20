382ac31ec03bac42989cb2a33430d2ed3629a606
Armando Peña Tamayo <arman2p284@gmail.com>
2023-11-20 17:17:51 -0500

    Fix: Change starting programming date

--- src/components/utilities/Experience.tsx
+++ src/components/utilities/Experience.tsx
@@ -1,9 +1,9 @@
-const YEAR_I_STARTED_PROGRAMMING = 2019
+const YEAR_I_STARTED_PROGRAMMING = 2018
 
 export default function Experience() {
   const yearsOfExperience = new Date().getFullYear() - YEAR_I_STARTED_PROGRAMMING
   return (<span className="text-purple-700 italic font-bold">
-    {yearsOfExperience} years of experience
+    +{yearsOfExperience} years of experience
   </span>
   )
 }
