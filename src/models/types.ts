3025e84da63e969a40fadf9561b45e7510b892b1
Armando Peña Tamayo <arman2p284@gmail.com>
2023-11-21 14:53:41 -0500

    Feat: Add Job global type

--- src/models/types.ts
+++ src/models/types.ts
@@ -2,3 +2,15 @@ export interface ExternalLink {
   url: string
   description: string
 }
+
+export interface Job {
+  id: string
+  imgUrl: string
+  imgDescription: string
+  title: string
+  startAt: Date
+  finishAt: Date | 'Current'
+  description: string
+  features: string[]
+  links: ExternalLink[]
+}
