5474e37ce68da9cbe74821ffc220aa31fabe752b
Armando Peña Tamayo <arman2p284@gmail.com>
2023-11-23 00:51:13 -0500

    Fix: Change navbar colors to brand purple

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <li >
       <a
         href={`/${url}`}
-        className={`block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>
+        className={`block py-2 px-3 text-slate-900 rounded hover:bg-slate-100 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:hover:bg-slate-700 dark:hover:text-white md:dark:hover:bg-transparent`}>
         {text}
       </a>
     </li >
@@ -63,7 +63,7 @@ export default function Navbar() {
             </g>
           </svg>
         </a>
-        <button onClick={() => setShowMenu(!showMenu)} type="button" className={`btn-burger ${showMenu ? 'open' : ''} p-2 w-10 h-10 flex flex-col items-center justify-center gap-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  `} aria-controls="navbar-default" aria-expanded="false">
+        <button onClick={() => setShowMenu(!showMenu)} type="button" className={`btn-burger ${showMenu ? 'open' : ''} p-2 w-10 h-10 flex flex-col items-center justify-center gap-2 text-sm text-slate-500 rounded-lg md:hidden hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-200  `} aria-controls="navbar-default" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
           <span className="burger-line border-slate-700 border-b w-full "></span>
           <span className="burger-line border-slate-700 border-b w-full "></span>
@@ -71,6 +71,7 @@ export default function Navbar() {
         </button>
         <div className={`${showMenu ? '' : 'hidden'} w-full md:block md:w-auto transition-all ease-out duration-300`}>
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
+            <NavLink text="Blog" url="blog" />
             <NavLink text="About me" url="about-me" />
           </ul>
         </div>
