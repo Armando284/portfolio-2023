33222900ab1ae63ffd51d1f43b87cbfaf3fe9d9c
Armando Peña Tamayo <arman2p284@gmail.com>
2023-11-23 21:42:34 -0500

    Fix: Update experience colors

--- src/components/shared/Experience.tsx
+++ src/components/shared/Experience.tsx
@@ -2,7 +2,7 @@ const YEAR_I_STARTED_PROGRAMMING = 2018
 
 export default function Experience() {
   const yearsOfExperience = new Date().getFullYear() - YEAR_I_STARTED_PROGRAMMING
-  return (<span className="text-purple-700 italic font-bold">
+  return (<span className="text-yellow-500 italic font-bold">
     +{yearsOfExperience} years of experience
   </span>
   )
