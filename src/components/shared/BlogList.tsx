e0384e427498dbf34222303157afc2c80c97b41a
Armando Peña Tamayo <arman2p284@gmail.com>
2023-11-25 19:18:41 -0500

    Fix: Change filter placeholder in blogs
    - From "Search" to "Filter" makes more sense

--- src/components/shared/BlogList.tsx
+++ src/components/shared/BlogList.tsx
@@ -38,7 +38,7 @@ export default function BlogList(props: MyBlogComponent) {
       <div className="w-full flex flex-wrap justify-between items-center mb-5 sm:mb-0">
         {props.children}
         <div className="w-max h-11 px-4 flex justify-between items-center gap-4 rounded-full border-slate-400 border-2">
-          <input type="search" name="param" placeholder="Search" onChange={debouncer()} className="border-0 bg-transparent outline-none" />
+          <input type="search" name="param" placeholder="Filter" onChange={debouncer()} className="border-0 bg-transparent outline-none" />
           <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
         </div>
       </div>
