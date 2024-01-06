75559d80d64cba6234c81ade26a912236a49540e
Armando Peña Tamayo <arman2p284@gmail.com>
2024-01-06 16:25:06 -0500

    Fix: Remove transparency from navbar
    Improves legibility

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
   )
 
   return (
-    <nav className="sticky top-0 z-50 bg-purple-950/80 shadow-md backdrop-blur-md">
+    <nav className="sticky top-0 z-50 bg-purple-950 shadow-md">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <svg
