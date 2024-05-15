dbebaac7fea7a1167717b025e5edf31850012974
Armando <72764913+Armando284@users.noreply.github.com>
2024-05-14 20:13:48 -0400

    Fix: Change Experience style to be more noticeable on the color palette

--- src/components/shared/Experience.tsx
+++ src/components/shared/Experience.tsx
@@ -2,7 +2,7 @@ const YEAR_I_STARTED_PROGRAMMING = 2018
 
 export default function Experience() {
   const yearsOfExperience = new Date().getFullYear() - YEAR_I_STARTED_PROGRAMMING
-  return (<span className="text-slate-200">
+  return (<span className="text-yellow-500">
     +{yearsOfExperience} years of experience
   </span>
   )
