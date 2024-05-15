29311cd42bfc7a0fdcd8537d219435d791b380ba
Armando <72764913+Armando284@users.noreply.github.com>
2024-05-15 00:31:04 -0400

    Fix: Changes download cv button to reduce CTA level

--- src/components/shared/DownloadButton.tsx
+++ src/components/shared/DownloadButton.tsx
@@ -1,44 +1,93 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 export default function DownloadButton() {
   const [isDownloading, setIsDownloading] = useState(false)
 
   const handleDownload = async () => {
-    console.log('handle download');
+    console.log('handle download')
 
     try {
-      setIsDownloading(true);
+      setIsDownloading(true)
 
-      const response = await fetch('Armando_Peña_CV_2023.pdf');
+      const response = await fetch('Armando_Peña_CV_2023.pdf')
       if (!response.ok) {
-        throw new Error('Error al descargar el archivo');
+        throw new Error('Error al descargar el archivo')
       }
 
-      const blob = await response.blob();
-      const url = URL.createObjectURL(blob);
+      const blob = await response.blob()
+      const url = URL.createObjectURL(blob)
 
-
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = 'Armando_Peña_CV_2023';
-      document.body.appendChild(link);
-      link.click();
+      const link = document.createElement('a')
+      link.href = url
+      link.download = 'Armando_Peña_CV_2023'
+      document.body.appendChild(link)
+      link.click()
 
       setTimeout(() => {
-        setIsDownloading(false);
-      }, 300);
+        setIsDownloading(false)
+      }, 300)
     } catch (error) {
-      console.error('Error al descargar el archivo:', error);
-      setIsDownloading(false);
+      console.error('Error al descargar el archivo:', error)
+      setIsDownloading(false)
     }
-  };
-
+  }
 
   return (
-    <button onClick={handleDownload}
+    <button
+      onClick={handleDownload}
       disabled={isDownloading}
-      className="w-48 h-11 px-4 text-yellow-500 transition-colors ease-in-out duration-300 flex items-center justify-center gap-1 border-yellow-500/50 hover:border-yellow-500 active:bg-slate-50 active:ring-2 active:ring-slate-50 border-2 rounded-md">
-      {isDownloading ? 'Downloading ...' : 'Download CV'}
+      className="w-24 h-11 text-slate-400 transition-colors ease-in-out duration-300 flex items-center justify-center">
+      ---
+      {isDownloading ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="w-8 h-8 icon icon-tabler icons-tabler-outline icon-tabler-loader animate-spin">
+          <path
+            stroke="none"
+            d="M0 0h24v24H0z"
+            fill="none"
+          />
+          <path d="M12 6l0 -3" />
+          <path d="M16.25 7.75l2.15 -2.15" />
+          <path d="M18 12l3 0" />
+          <path d="M16.25 16.25l2.15 2.15" />
+          <path d="M12 18l0 3" />
+          <path d="M7.75 16.25l-2.15 2.15" />
+          <path d="M6 12l-3 0" />
+          <path d="M7.75 7.75l-2.15 -2.15" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="w-8 h-8 icon icon-tabler icons-tabler-outline icon-tabler-file-cv">
+          <path
+            stroke="none"
+            d="M0 0h24v24H0z"
+            fill="none"
+          />
+          <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+          <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+          <path d="M11 12.5a1.5 1.5 0 0 0 -3 0v3a1.5 1.5 0 0 0 3 0" />
+          <path d="M13 11l1.5 6l1.5 -6" />
+        </svg>
+      )}
+      ---
     </button>
   )
 }
