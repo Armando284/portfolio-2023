91e9e032effb2d15d20ad8eef3100d2429d6a27c
Armando <72764913+Armando284@users.noreply.github.com>
2024-06-07 01:46:16 -0400

    Feat: Improving work experience content

--- src/lib/jobs.ts
+++ src/lib/jobs.ts
@@ -1,15 +1,35 @@
 import type { Job } from '@/models/types'
 
 export const jobs: Job[] = [
+  {
+    id: crypto.randomUUID(),
+    imgUrl: '',
+    imgDescription: '',
+    title: 'FullStack Developer @ Secret',
+    startAt: new Date('2024-03-15'),
+    finishAt: 'Current',
+    description: `As a Semi-Senior Fullstack Developer, I have been working on a comprehensive project that involves developing and maintaining two Angular-based frontends using Tailwind CSS: one for content administration and another for user-facing interactions. My responsibilities extend to integrating the backend for frontend (BFF) architecture using Lumen and managing dynamic content through Squidex CMS. Additionally, we use Keycloak for authentication processes.
 
+In this role, I have been responsible for adding new features to enhance functionality and user experience, fixing bugs to ensure smooth and error-free operations, and making significant improvements to the UI and UX for better engagement. I have also focused on code refactoring to improve maintainability and overall performance.
+
+A significant part of my work has involved performance optimization, particularly through image optimization and refining search and sort algorithms. On the SEO front, I have enhanced the website's search engine visibility by adding alt information to images, incorporating meta keywords, and improving the semantics of the HTML structure.
+
+One of the main challenges in this project has been the frequent changes in development team members, which led to variations in code style and quality over time. To address this, we implemented code style guides and linters to ensure consistency and conducted daily standups to discuss and resolve issues promptly. Another major challenge was the implementation of Squidex, which does not support reference data sorting. This limitation required extensive investigation and the development of custom solutions to meet project requirements.`,
+    features: ['Angular', 'Tailwind', 'SPA', 'Lumen', 'Squidex', 'Keycloak'],
+    links: [],
+  },
   {
     id: crypto.randomUUID(),
     imgUrl: 'cuban_engineer.webp',
     imgDescription: 'Cuban Engineer Website',
     title: 'Frontend Developer @ Cuban Engineer',
     startAt: new Date('2023-08-1'),
-    finishAt: 'Current',
-    description: `Updated company’s website framework, Angular, from version 10 to version 15 and deprecated dependencies. Improved performance by 15% by replacing or updating deprecated dependencies and refactoring old code into newer standards. Improved backward version compatibility and reduced merge conflicts by following a cascade versioning system of git branches for each version update`,
+    finishAt: new Date('2023-11-20'),
+    description: `As a Frontend Developer, I worked on a Single Page Application (SPA) built with Angular for a CV generator tailored to developers and IT professionals. The project involved handling extensive career-related data. A significant challenge was that the Angular version in use was outdated, and many dependencies were deprecated.
+
+My primary responsibility was updating Angular from version 10 to version 15 and addressing deprecated dependencies. This task required substantial effort and numerous unit tests to ensure development security. By replacing or updating deprecated dependencies and refactoring old code to meet modern standards, I improved the performance for the First Contentfull Paint FCP and total load time by 15%.
+
+Additionally, I implemented a cascade versioning system in Git to manage version updates, which enhanced backward compatibility and reduced merge conflicts.`,
     features: ['Angular', 'SPA'],
     links: [
       {
@@ -17,7 +37,6 @@ export const jobs: Job[] = [
         description: 'Cuban Engineer website',
       },
     ],
-
   },
   {
     id: crypto.randomUUID(),
@@ -34,7 +53,6 @@ export const jobs: Job[] = [
         description: 'Wassermeloni website',
       },
     ],
-
   },
   {
     id: crypto.randomUUID(),
@@ -51,7 +69,5 @@ export const jobs: Job[] = [
         description: 'Imagined Earth website',
       },
     ],
-
   },
-
 ]
